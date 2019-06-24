07d51b4e8cbcd4c3775a0746140f29c28d372702
Ngichu <brianngichu@gmail.com>
2019-06-24 10:48:52 +0300

    created two classes for news & articles

---
+++ app/models.py
@@ -0,0 +1,23 @@
+class News:
+    '''
+    all news class to define news objects
+    '''
+    def __init__(self, name, url,description, country,category, id):
+        self.name = name
+        self.url = url
+        self.description = description
+        self.country = country
+        self.category = category
+        self.id = id
+
+class Articles:
+    '''
+    articles class to define article objects
+    '''
+    def __init__(self, title, description, urlToImage, publishedAt, author, url):
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.author = author
+        self.url = url
