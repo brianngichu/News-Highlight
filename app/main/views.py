08dc8d9b14ece94900cc2e720f5f931b050b0ddf
Ngichu <brianngichu@gmail.com>
2019-06-24 11:17:31 +0300

    view function for articles

--- app/main/views.py
+++ app/main/views.py
@@ -29,3 +29,14 @@ def news(id):
     
     return render_template('index.html', news = news)
 
+
+@main.route('/articles/<source_id>')
+def articles(source_id):
+    '''
+    function that returns articles by source id
+    '''
+
+    article_source = get_articles(source_id)
+    title = f'{source_id}| Articles'
+    return render_template('articles.html',title = title, name = source_id, news = article_source )
+
